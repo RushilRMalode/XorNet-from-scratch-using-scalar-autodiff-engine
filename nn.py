fa0d4ed4ae3dfd025e6d6b5043d4f10ebd832a9c
Rushil <rushilrmalode@gmail.com>
2026-06-09 23:57:14 +0530

    Added class layer and fixed bugs in class neuron

--- nn.py
+++ nn.py
@@ -1,9 +1,9 @@
 from engine import Value
-
+import random
 class neuron:
     #activation (weights*inputs + bias) 
     def __init__(self,n_inputs,operation='ReLU'):  
-        self.weights = [Value(0)]*n_inputs
+        self.weights = [Value(random.uniform(-1,1)) for _ in range(n_inputs)]
         if operation == 'ReLU':
             self.activation = Value.relu
         elif operation == 'tanh':
@@ -11,14 +11,24 @@ class neuron:
         elif operation == 'sigmoid':
             self.activation = Value.sigmoid
         else:
-            return NameError
+            return ValueError('Activation not implemented')
         
-        self.bias = Value(0)
+        self.bias = Value(random.uniform(-1,1))
     
     def __call__(self,x):
-        out = Value(x*self.weights.data+self.bias.data,_children=(self.weights,self.bias),_op= '+')
+        x = [xi if isinstance(xi,Value) else Value(xi) for xi in x]
+        out = (sum((xi*wi for xi,wi in zip(x,self.weights)),self.bias))
         out = self.activation(out)
         return out
     def parameters(self):
-        return self.weights
+        return self.weights + [self.bias]
     
+class Layer:
+    def __init__(self,width,n_inputs,operation='ReLU'):
+        self.neurons = [neuron(n_inputs,operation) for _ in range(width)]
+    def __call__(self,x):
+        x = [xi if isinstance(xi,Value) else Value(xi) for xi in x]
+        out = [n(x) for n in self.neurons]
+        return out
+    def parameters(self):
+        return [n.parameters() for n in self.neurons]
